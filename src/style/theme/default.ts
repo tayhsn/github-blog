2e6a40df282f04f1e6fe4a647460a2cb35bd61ef
Tayanne <tayh.snovaes@gmail.com>
2022-10-21 14:51:48 -0300

    add theme

---
+++ src/style/theme/default.ts
@@ -0,0 +1,23 @@
+const defaultTheme = {
+  colors: {
+    'base-title': '#E7EDF4',
+    'base-subtitle': '#C4D4E3',
+    'base-text': '#AFC2D4',
+    'base-span': '#7B96B2',
+    'base-label': '#3A536B',
+    'base-border': '#1C2F41',
+    'base-post': '#112131',
+    'base-profile': '#0B1B2B',
+    'base-background': '#071422',
+    'base-input': '#040F1A',
+    blue: '#3294F8',
+  },
+  textSizes: {
+    'title-l': '1.5rem',
+    'title-m': '1.25rem',
+    'title-s': '1.125rem',
+    'text-m': '1rem',
+    'text-s': '0.875rem',
+    'text-xs': '0.75rem',
+  },
+}
